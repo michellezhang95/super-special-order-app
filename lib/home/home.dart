58244fd181dfbf7c5095d33d0490db80961e5e2c
michellezhang95 <michelle.zhang56@gmail.com>
2020-07-30 10:28:16 +1200

    list tile UI and coffee brew model

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:orderingapp/services/auth.dart';
+import 'package:orderingapp/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:orderingapp/home/brew_list.dart';
+import 'package:orderingapp/models/brew.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Super Order'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () async {
-              await _auth.signOut();
-            },
-            icon: Icon(Icons.person),
-            label: Text('logout'),
-          )
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Super Order'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:orderingapp/models/brew.dart';
 
 class DatabaseService {
   //collection reference
@@ -14,5 +15,21 @@ class DatabaseService {
       'name': name,
       'strength': strength,
     }); // pass id associated with user
+  }
+
+  //brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+          name: doc.data['name'] ?? '',
+          strength: doc.data['strength'] ?? 0,
+          sugars: doc.data['sugars'] ?? '0');
+    }).toList();
+  }
+
+  //get brews stream
+  //QuerySnapshot is snapshot of firestore database at point in time
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
